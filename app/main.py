d35105944763b174204d0bca89eb95367393eba1
danila nikitin <danila.n2015@yandex.ru>
2026-06-10 19:27:28 +0300

    feat: add create user request

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import logging
 from flask import Flask, jsonify, request
+import db
+from pydantic import ValidationError
+from models import User
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,6 +14,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return jsonify({"message": "Hello, World!"})
+
+
+@app.route("/api/users", methods=["POST"])
+def create_user():
+    data = request.get_json(silent=True) or {}
+    try:
+        user = User.model_validate(
+            {"id": db._next_id, **data},
+            context={"users": db._users},
+        )
+    except ValidationError as e:
+        return jsonify({"error": e.errors()}), 400
+    db._users.append(user)
+    db._next_id += 1
+    logger.info("Created user id=%d name=%s", user.id, user.name)
+    return jsonify(user.model_dump()), 201
 
 
 if __name__ == "__main__":
